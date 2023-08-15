07aa9ee385732c3ecd4f0d108bf0dcfb64b16c4d
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-14 22:06:45 -0600

    Se declaran variables

--- js/app.js
+++ js/app.js
@@ -4,4 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
         asunto: '',
         mensaje: ''
     }
+
+  
+    // Variables
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 });
